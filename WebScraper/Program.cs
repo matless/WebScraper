865e6fea4b075e868a5a662925bd3d6ba934c015
komp <komp@DESKTOP-A8RDDA2>
2023-08-31 23:15:58 +0200

    Add project files.

---
+++ WebScraper/Program.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Send get request to weather.com
+            String url = "https://weather.com/pl-PL/pogoda/dzisiaj/l/6dcbdf0ed6150319cfcaf3d87107792c5b58dcf6ed512833c1d5930af478a5e5";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+
+            // Get the temperature
+            var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempValue--MHmYY\']");
+            var temperature = temperatureElement.InnerText.Trim();
+            Console.WriteLine( "Temperature: " + temperature);
+
+            // Get the conditions
+            var conditionElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='CurrentConditions--phraseValue--mZC_p']");
+            var conditions = conditionElement.InnerText.Trim();
+            Console.WriteLine("Conditions: " + conditions);
+
+            // Get the location
+            var locationElement = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='CurrentConditions--location--1YWj_']");
+            var location = locationElement.InnerText.Trim();
+            Console.WriteLine("Location: " + location);
+
+        }
+    }
+}
